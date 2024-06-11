c707e6c4bb0a82a60233d7c8952da6bddd079ad8
jlaron230 <jlaron230@hotmail.fr>
2024-06-11 14:30:25 +0200

    Ajout de commentaires

--- backend/src/models/DeadlineManager.js
+++ backend/src/models/DeadlineManager.js
@@ -5,10 +5,11 @@ class DeadlineManager extends AbstractManager {
     super({ table: "deadline" });
   }
 
+  // Function to insert a new deadline into the database
   insert(deadline) {
     return this.database.query(`INSERT INTO ${this.table} 
       (description, title, debut, end, statut, id_user) 
-      VALUES (?, ?, ?, ?, ?)`, [
+      VALUES (?, ?, ?, ?, ?, ?)`, [
         deadline.description,
         deadline.title,
         deadline.debut,
@@ -18,20 +19,28 @@ class DeadlineManager extends AbstractManager {
     ]);
   }
   
-
+  // Function to update an existing deadline in the database
   update(deadline) {
     return this.database.query(
-        `UPDATE ${this.table} SET 
+      `UPDATE ${this.table} SET 
         description = ?, 
         title = ?, 
         debut = ?, 
         end = ?, 
-        statut = ? 
+        statut = ?, 
         id_user = ? 
-        WHERE id = ?`,
-        [deadline.description, deadline.title, deadline.debut, deadline.end, deadline.statut, deadline.id_user]
-      );
-    }
+      WHERE id = ?`,
+      [
+        deadline.description, 
+        deadline.title, 
+        deadline.debut, 
+        deadline.end, 
+        deadline.statut, 
+        deadline.id_user, 
+        deadline.id
+      ]
+    );
   }
+}
 
 module.exports = DeadlineManager;
--- backend/src/models/Quote_CategoryManager.js
+++ backend/src/models/Quote_CategoryManager.js
@@ -1,17 +1,18 @@
 const AbstractManager = require("./AbstractManager");
 
-class Quote_Category extends AbstractManager {
+class QuoteCategory extends AbstractManager {
   constructor() {
     super({ table: "quote_category" });
   }
 
+  // Function to insert a new quote category into the database
   insert(quote_category) {
     return this.database.query(`insert into ${this.table} (title) values (?)`, [
-        quote_category.title,
+      quote_category.title,
     ]);
   }
-  
 
+  // Function to update an existing quote category in the database
   update(quote_category) {
     return this.database.query(
       `update ${this.table} set title = ? where id = ?`,
@@ -20,4 +21,4 @@ class Quote_Category extends AbstractManager {
   }
 }
 
-module.exports = Quote_Category;
+module.exports = QuoteCategory;
