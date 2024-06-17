76d68550b12375d2fc323f6a6a0d8f4828ad97a2
jlaron230 <jlaron230@hotmail.fr>
2024-06-17 17:24:42 +0200

    add Contact page and privacy-policy, NotFound, legalNotice

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
+    "../frontend/src/index.css",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.css",
   ],
   theme: {
-    extend: {},
+    extend: {    
+      colors: {
+        theme1: '#CF9845',
+        theme2: '#C97D50',
+        themeBlack: '#080808',
+        themeWhite: '#FFFFFF',
+        themeGray: '#697077'
+      },
+      fontFamily: {
+        serif: ['Laila', 'serif'],
+        serif: ['Imprima', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
