7a32fbda100f5581ca7aceb0a534ddc4d8f6f86c
CalliEtc <callipter@gmail.com>
2024-06-19 10:19:07 +0200

    debut of the calendar page : MyCalendar components have been use and it worked

--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -1,6 +1,17 @@
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' 
+import CalendarComponents from '@components/Calendar/CalendarComponents'
+import NavBar from "@components/NavBar/NavBar";
+import Footer from "@components/Footer/Footer";
+
+
 export default function Calendar (){
     return (
        <>
+       <NavBar />
+       <CalendarComponents />
+       <Footer/>
        </> 
     )
 }
+
