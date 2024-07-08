074eca82c63ce6fd87aaeada34d67089e309033d
DaSilva <faysoil.chaambani@outlook.fr>
2024-07-08 17:07:09 +0200

    fix: click pagination

--- frontend/src/components/Pagination/Pagination.jsx
+++ frontend/src/components/Pagination/Pagination.jsx
@@ -12,11 +12,11 @@ function Pagination({ pageCount, onPageChange }) {
       previousLabel="<"
       renderOnZeroPageCount={null}
       containerClassName="flex flex-wrap justify-center space-x-2 mt-4"
-      pageClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200"
+      pageClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center" // Adjusted
       activeClassName="bg-blue-500 text-black"
-      previousClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200"
-      nextClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200"
-      breakClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200"
+      previousClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center" // Adjusted
+      nextClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center" // Adjusted
+      breakClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center" // Adjusted
       disabledClassName="bg-gray-200 cursor-not-allowed"
     />
   );
