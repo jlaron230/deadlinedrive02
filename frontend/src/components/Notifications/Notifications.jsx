2a80c8e5c0c2cf2558940579ff1784e107539040
ANINACH <aninach.beg@gmail.com>
2024-07-08 11:04:53 +0200

    added style

--- frontend/src/components/Notifications/Notifications.jsx
+++ frontend/src/components/Notifications/Notifications.jsx
@@ -90,7 +90,7 @@ function NotificationsComponent({ isOpen, onClose }) {
                             className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
                             onClick={onClose}
                         >
-                            Close
+                            Fermer
                         </button>
                     </div>
                 </motion.div>
