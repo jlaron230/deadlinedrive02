3c9143e47daf13220e36118878caa3397ff79697
DaSilva <faysoil.chaambani@outlook.fr>
2024-07-08 17:37:38 +0200

    fix: container of click button

--- frontend/src/components/Pagination/Pagination.jsx
+++ frontend/src/components/Pagination/Pagination.jsx
@@ -12,11 +12,12 @@ function Pagination({ pageCount, onPageChange }) {
       previousLabel="<"
       renderOnZeroPageCount={null}
       containerClassName="flex flex-wrap justify-center space-x-2 mt-4"
-      pageClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center" // Adjusted
-      activeClassName="bg-blue-500 text-black"
+      pageClassName="bg-white border border-gray-300 rounded cursor-pointer hover:bg-gray-200 flex items-center justify-center" // Adjusted
+      activeClassName="bg-blue-400 text-black"
       previousClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center" // Adjusted
       nextClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center" // Adjusted
       breakClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center" // Adjusted
+      pageLinkClassName="px-3"
       disabledClassName="bg-gray-200 cursor-not-allowed"
     />
   );
