7c89bb5b5543d8cb0043047a836bec9e346c4f24
DaSilva <faysoil.chaambani@outlook.fr>
2024-07-09 09:39:03 +0200

    delete comments

--- frontend/src/components/Pagination/Pagination.jsx
+++ frontend/src/components/Pagination/Pagination.jsx
@@ -12,11 +12,11 @@ function Pagination({ pageCount, onPageChange }) {
       previousLabel="<"
       renderOnZeroPageCount={null}
       containerClassName="flex flex-wrap justify-center space-x-2 mt-4"
-      pageClassName="border border-gray-300 rounded cursor-pointer hover:bg-gray-200 flex items-center justify-center" // Adjusted
+      pageClassName="border border-gray-300 rounded cursor-pointer hover:bg-gray-200 flex items-center justify-center"
       activeClassName="bg-blue-400 text-black"
-      previousClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center" // Adjusted
-      nextClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center" // Adjusted
-      breakClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center" // Adjusted
+      previousClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center"
+      nextClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center"
+      breakClassName="bg-white border border-gray-300 rounded px-3 py-1 cursor-pointer hover:bg-gray-200 flex items-center justify-center"
       pageLinkClassName="px-3"
       disabledClassName="bg-gray-200 cursor-not-allowed"
     />
